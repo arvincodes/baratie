771a7d8658f0464e304c6b6c0b7f94bdc7f6f725
Arvin Garcia <arvin.garcia.dev@gmail.com>
2024-11-26 14:58:49 +0800

    Display home content when clicking home button
    Transferred the HTML content in template.html to home.js to display
    content only when the home button is clicked.

---
+++ src/home.js
@@ -0,0 +1,122 @@
+import instagramIcon from "./images/instagram.svg";
+import twitterIcon from "./images/twitter.svg";
+import facebookIcon from "./images/facebook.svg";
+
+export const home = `
+    <div class="wrapper">
+      <section class="home-hero">
+        <div class="hero-img-container"></div>
+        <div class="hero-info-container">
+          <h1 class="hero-heading">Sail the high seas of flavor.</h1>
+          <p class="subheading">Discover a world of flavor, where every bite is an adventure.</p>
+          <div class="hero-btns">
+            <button class="buy-btn">Buy Now</button>
+            <button class="how-btn">How to Order</button>
+          </div>
+        </div>
+      </section>
+      <section class="key-points-container">
+        <div class="key-point">
+          <div class="key-point-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M3 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061A1.125 1.125 0 0 1 3 16.811V8.69ZM12.75 8.689c0-.864.933-1.406 1.683-.977l7.108 4.061a1.125 1.125 0 0 1 0 1.954l-7.108 4.061a1.125 1.125 0 0 1-1.683-.977V8.69Z" />
+            </svg>          
+          </div>
+          <div class="key-point-content">
+            <h2>Fast Delivery</h2>
+            <p>Like a Straw Hat Pirate raid, your food will arrive in 1 to 2 hours—swift and hassle-free!</p>
+          </div>
+        </div>
+        <div class="key-point">
+          <div class="key-point-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+            </svg>                  
+          </div>
+          <div class="key-point-content">
+            <h2>Fresh Food</h2>
+            <p>As fresh as the sea breeze at the Grand Line, your food will be delivered hot and 100% fresh to your home!</p>
+          </div>
+        </div>
+        <div class="key-point">
+          <div class="key-point-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg>                  
+          </div>
+          <div class="key-point-content">
+            <h2>Free Delivery</h2>
+            <p>Just like the treasure Luffy seeks, your delivery comes with no extra cost—order and enjoy!</p>
+          </div>
+        </div>
+        <div class="key-point">
+          <div class="key-point-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg>                  
+          </div>
+          <div class="key-point-content">
+            <h2>Signature Cuisine</h2>
+            <p>Each dish is created with Sanji’s signature touch, bringing you the taste of the All Blue!</p>
+          </div>
+        </div>
+      </section>
+      <section class="best-delivered-container">
+        <h1>Best Delivered Categories</h1>
+        <p class="subheading">Here are some of our best delivered food categories across the East Blue!</p>
+        <div class="best-delivered-items-container">
+          <div class="food-category">
+            <div class="food-img burger"></div>
+            <div class="food-content">
+              <h2>Sea King Burger</h2>
+              <p>Order Now ></p>
+            </div>
+          </div>
+          <div class="food-category">
+            <div class="food-img ramen"></div>
+            <div class="food-content">
+              <h2>Grand Line Ramen</h2>
+              <p>Order Now ></p>
+            </div>
+          </div>
+          <div class="food-category">
+            <div class="food-img fries"></div>
+            <div class="food-content">
+              <h2>Golden Fries</h2>
+              <p>Order Now ></p>
+            </div>
+          </div>
+          <div class="food-category">
+            <div class="food-img dessert"></div>
+            <div class="food-content">
+              <h2>Devil Fruit Dessert</h2>
+              <p>Order Now ></p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <footer>
+      <div class="footer-main">
+        <div class="footer-menu">
+          <h3>Menu</h3>
+          <p>Home</p>
+          <p>Offers</p>
+          <p>Service</p>
+          <p>About Us</p>
+        </div>
+        <div class="footer-contact">
+          <h3>Contact</h3>
+          <p>+123 456 789</p>
+          <p>Explore</p>
+          <p>info@baratie.com</p>
+          <p>1245, East Blue, JPN</p>
+        </div>
+      </div>
+      <div class="social-media">
+        <img src="${instagramIcon}" class="social-media-icon">
+        <img src="${twitterIcon}" class="social-media-icon">
+        <img src="${facebookIcon}" class="social-media-icon">
+      </div>
+    </footer>
+`
--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 import "./styles.css"
-import { greeting } from "./greeting.js"
-
-console.log(greeting)
-console.log("test")
+import { home } from "./home.js"
+import { menu } from "./menu.js"
+import { about } from "./about.js"
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const sidebar = document.querySelector('.sidebar');
 const overlay = document.querySelector('.overlay');
 const xBtn = document.querySelector('.x-btn')
 
+const content = document.getElementById('content')
+const homeBtn = document.querySelector('.home-btn')
+
 function toggleSidebar() {
   sidebar.classList.toggle('active');
   overlay.classList.toggle('active');
 }
 
+function displayContent(contentCategory) {
+  if (contentCategory === 'home') {
+    content.innerHTML = home
+  } else if (contentCategory === 'menu') {
+    content.innerHTML = menu
+  } else if (contentCategory === 'about') {
+    content.innerHTML = about
+  }
+}
+
+// function displayContent() {
+//     content.innerHTML = home
+// }
+
 hamburgerMenu.addEventListener('click', toggleSidebar);
 overlay.addEventListener('click', toggleSidebar);
 xBtn.addEventListener('click', toggleSidebar)
+homeBtn.addEventListener('click', () => displayContent('home'))
+// document.addEventListener('DOMContentLoaded', () => {
+//   displayContent();
+// });
