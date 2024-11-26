bcf0a72636f564e829eb8de9ec7b9f015e7763c1
Arvin Garcia <arvin.garcia.dev@gmail.com>
2024-11-26 10:44:07 +0800

    Add functional hamburger menu

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import { greeting } from "./greeting.js"
 
 console.log(greeting)
 console.log("test")
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const sidebar = document.querySelector('.sidebar');
+const overlay = document.querySelector('.overlay');
+const xBtn = document.querySelector('.x-btn')
+
+function toggleSidebar() {
+  sidebar.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+hamburgerMenu.addEventListener('click', toggleSidebar);
+overlay.addEventListener('click', toggleSidebar);
+xBtn.addEventListener('click', toggleSidebar)
